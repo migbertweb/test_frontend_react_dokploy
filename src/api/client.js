a699076a0461bb2f44f21bc70c6d203fe2d94d87
migbertweb <migbertyanez@disroot.org>
2026-01-02 20:52:01 -0300

    prueba de deploy1

--- src/api/client.js
+++ src/api/client.js
@@ -3,6 +3,7 @@
  * GitHub: https://github.com/migbertweb
  * Description: Configuración del cliente Axios para realizar peticiones a la API backend.
  * Incluye interceptores para agregar el token JWT y manejar errores 401.
+ * agregando comentario para probar el deploy en Dokploy
  */
 import axios from 'axios';
 import useAuthStore from '../store/authStore';
